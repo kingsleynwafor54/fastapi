7365399dc01372672e3b2e8587965727e198e24c
kingsleynwafor54 <kingsleynwafor54@gmail.com>
2026-01-25 19:23:27 +0100

    add alembic and requirements.txt4

--- app/main.py
+++ app/main.py
@@ -43,15 +43,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# app.include_router(post.router)
-# app.include_router(user.router)
-# app.include_router(auth.router)
-# app.include_router(vote.router)
-
-app.include_router(auth.router, prefix="/login")   # or "/auth" if multiple auth endpoints
-app.include_router(post.router, prefix="/posts")
-app.include_router(user.router, prefix="/users")
-app.include_router(vote.router, prefix="/vote")
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(auth.router)
+app.include_router(vote.router)
 
 # @app.get("/")
 # async def root(db:Session=Depends(get_db)):
